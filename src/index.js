6367aa68c272cde885634271b4fc5df4e367426e
PRE90 <erian.pr@icloud.com>
2016-09-15 16:04:09 -0700

    Frame for video list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list'
 const API_KEY = '';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos: videos });
+		});
+	}
+
+	render () {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={ this.state.videos }/>
+			</div>
+		);
+	}
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
