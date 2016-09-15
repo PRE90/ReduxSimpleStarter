26e472abbfb98866b34b6ea6558cec11c2e0defd
PRE90 <erian.pr@icloud.com>
2016-09-15 16:56:38 -0700

    Apply basic styling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
-const API_KEY = '';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCXLSs5eGzLb0KbeHSe4Zcz6Itw1EErOsA';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
-		YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos: videos });
+		YTsearch({key: API_KEY, term: 'puppy'}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
@@ -20,7 +27,11 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={ this.state.videos }/>
+				<VideoDetail video={ this.state.selectedVideo }/>
+				<VideoList 
+					onVideoSelect={ selectedVideo => this.setState(
+						{ selectedVideo }) }
+					videos={ this.state.videos } />
 			</div>
 		);
 	}
